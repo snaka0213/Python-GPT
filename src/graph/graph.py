47f2cd52a5f2153edf4283e5628d6d7f9187e701
Shinichiro Nakamura <nakamura.s.az@gmail.com>
2019-07-16 18:46:16 +0900

    Update for minor changes

--- src/graph/graph.py
+++ src/graph/graph.py
@@ -2,10 +2,10 @@
 
 class OrientedGraph(object):
     def __init__(self, num_of_nodes: int):
-        self._nodes = [i for i in range(num_of_nodes)]
-        self._edges = [[] for i in range(num_of_nodes)]
+        self.nodes = [i for i in range(num_of_nodes)]
+        self.edges = [[] for i in range(num_of_nodes)]
 
     def add_edge(self, v, w):
         # add edge: node v -> node w
-        self._edges[v].append(w)
+        self.edges[v].append(w)
 
