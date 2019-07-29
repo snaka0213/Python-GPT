3b2c18f22ed21aa5381bd0b5d482b17f0aed4c4c
Shinichiro Nakamura <nakamura.s.az@gmail.com>
2019-07-29 17:55:20 +0900

    make it better

--- src/scripts/learning/learn_hyperplane.py
+++ src/scripts/learning/learn_hyperplane.py
@@ -15,13 +15,17 @@ epsilon = settings.Epsilon
 sample_size   = settings.SampleSize
 learning_rate = settings.InitialLearningRate
 
-# sigmoid function
+# sigmoid
 def sigma(z):
     return 1/(1+np.exp(-z))
 
+# log of sigmoid
+def log_sigmoid(*args):
+    return sum([z if -z > 700 else np.log(sigma(z)) for z in args])
+
 # the i-th standard_basis in n-dim Euclidian space
-def standard_basis(n: int, i: int) -> np.ndarray:
-    e = np.zeros(n, dtype=int); e[i] = 1
+def standard_basis(n: int, i: int, v: np.float64) -> np.ndarray:
+    e = np.zeros(n, dtype=int); e[i] = v
     return e
 
 # if sample in the same posision as `normal`: +1; else: -1
@@ -40,21 +44,8 @@ class Loss(object):
         val = 0
         feature_vector = feature_vector_dict[feature_index]
         constant = two_valued_classifier(feature_vector, normal)
-        for index in knn_list:
-            knn_vector = feature_vector_dict[index]
-            z = constant * knn_vector @ normal.T
-            if -z > 700:
-                val += z
-            else:
-                val += np.log(sigma(z))
-
-        for random_index in samples_index:
-            random_vector = feature_vector_dict[random_index]
-            z = -constant * random_vector @ normal.T
-            if -z > 700:
-                val += z
-            else:
-                val += np.log(sigma(z))
+        val += sum([log_sigmoid(constant * feature_vector_dict[index] @ normal.T) for index in knn_list])
+        val += sum([log_sigmoid(-constant * feature_vector_dict[random_index] @ normal.T) for random_index in samples_index])
 
         # regularization
         val -= Lambda * np.linalg.norm(normal, 1)
@@ -62,9 +53,9 @@ class Loss(object):
 
     def gradient(self, knn_list, feature_index, feature_vector_dict, samples_index, normal):
         epsilon, Lambda, M = self.epsilon, self.Lambda, normal.size
-        return (1/epsilon)*np.array([
-            self.value(knn_list, feature_index, feature_vector_dict, samples_index, normal + epsilon * standard_basis(M, i)) \
-            - self.value(knn_list, feature_index, feature_vector_dict, samples_index, normal) for i in range(M)
+        return np.array([
+            (self.value(knn_list, feature_index, feature_vector_dict, samples_index, normal + standard_basis(M, j, epsilon)) \
+            - self.value(knn_list, feature_index, feature_vector_dict, samples_index, normal))*(1/epsilon) for j in range(M)
         ])
 
 
@@ -77,27 +68,46 @@ class LearnHyperPlane(object):
         self.normal = init_normal # normal vector of hyperplane
 
     def learn(self, debug=False):
+        M = self.M
         knn = self._knn
         inverted_index = self._inverted_index
         feature_vector_dict = self._feature_vector_dict
-
-        # AdaGrad
-        loss = Loss(epsilon=epsilon, Lambda=Lambda)
-        optimizer = AdaGrad(learning_rate=learning_rate)
+        samples_index = random.sample(feature_vector_dict.keys(), sample_size)
 
         # an epoch
-        def learn_in_epoch(i, params, grads):
-            samples_index = random.sample(feature_vector_dict.keys(), sample_size)
-            index_list = list(set(inverted_index.get(i))&set(feature_vector_dict.keys()))
-            knn_list = knn.get_index(feature_vector_dict[i], index_list)
-            grads["normal"] += loss.gradient(knn_list, i, feature_vector_dict, samples_index, self.normal)
+        def learn_in_epoch(i, params, optimizer):
+            normal = params["normal"]
+            knn_list = knn.get_index(feature_vector_dict[i], list(set(inverted_index.get(i))&set(feature_vector_dict.keys())))
+            # refactored version
+            constant = two_valued_classifier(feature_vector_dict[i], normal)
+            normal_clness = {p: constant*feature_vector_dict[p]@normal.T for p in knn_list}
+            sample_clness = {p: -constant*feature_vector_dict[p]@normal.T for p in samples_index}
+
+            norm_perturbed_along = [
+                np.linalg.norm(normal+standard_basis(M, q, epsilon), 1) for q in range(M)
+            ]
+            loss_perturbed_along = [
+                -log_sigmoid(*[normal_clness[p]+epsilon*constant*feature_vector_dict[p][q] for p in knn_list]) \
+                -log_sigmoid(*[sample_clness[p]-epsilon*constant*feature_vector_dict[p][q] for p in samples_index]) \
+                +Lambda*norm_perturbed_along[q] for q in range(M)
+            ]
+            loss_at_point = -log_sigmoid(*normal_clness) - log_sigmoid(*sample_clness) + Lambda*np.linalg.norm(normal, 1)
+            grad = np.array([(1/epsilon)*(loss_perturbed_along[q]-loss_at_point) for q in range(M)])
+
+            grads = {"normal": grad}
             optimizer.update(params, grads)
-
-
-        for epoch in range(Epoch):
-            # mini batch learning
-            params = {"normal": self.normal}
-            grads = {"normal": np.zeros_like(self.normal)}
-            Parallel(n_jobs=Threads, verbose=5)(delayed(learn_in_epoch)(i, params, grads) for i in feature_vector_dict.keys())
             if debug:
-                print("Epoch {}: Done".format(epoch))
+                loss = Loss(epsilon, Lambda)
+                value = loss.value(knn_list, i, feature_vector_dict, samples_index, params["normal"])
+                print("Index: {}, Loss: {}".format(i, value))
+
+        def job(i, params):
+            # AdaGrad
+            optimizer = AdaGrad(learning_rate=learning_rate)
+            for epoch in range(Epoch):
+                if debug:
+                    print("### Epoch: {} ###".format(epoch))
+                learn_in_epoch(i, params, optimizer)
+
+        params = {"normal": self.normal}
+        Parallel(n_jobs=Threads, verbose=5)(delayed(job)(i, params) for i in feature_vector_dict.keys())
